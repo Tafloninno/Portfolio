0c74163658c4f5eb7f30da46d144502bd0d1c07b
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-01-31 01:47:44 +0300

    Made changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const navMenu = document.querySelector('.mobile_menu');
 humburger.addEventListener('click', () => {
   humburger.classList.toggle('active');
   navMenu.classList.toggle('active');
-//   console.log(humburger);
+
 });
 document.querySelectorAll('.nav_link').forEach((n) => n.addEventListener('click', () => {
   humburger.classList.remove('active');
