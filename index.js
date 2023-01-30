f5192ae65eb98c3910dbc73c0fe410e5f8736cc7
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-01-30 18:09:48 +0300

    update index.html file add hambuger menu add index.js file update style.css file

---
+++ index.js
@@ -0,0 +1,13 @@
+const humburger = document.querySelector('.humburger_menu');
+const navMenu = document.querySelector('.mobile_menu');
+
+humburger.addEventListener('click', () => {
+  humburger.classList.toggle('active');
+  navMenu.classList.toggle('active');
+//   console.log(humburger);
+});
+document.querySelectorAll('.nav_link').forEach((n) => n.addEventListener('click', () => {
+  humburger.classList.remove('active');
+  navMenu.classList.remove('active');
+
+}));
